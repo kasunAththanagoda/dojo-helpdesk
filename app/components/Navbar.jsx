e07c0b0aa6472ded801fef212246b626a10e8912
kasun Aththanagoda <kasun@icanlk.com>
2024-06-01 16:03:23 +0530

    vid 5

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import Link from 'next/link'
+import Logo from '@/app/components/dojo-logo.png'
+import Image from 'next/image'
 
 
 export default function News() {
     return (
         <nav>
+            <Image 
+            src={Logo}
+            alt="Dojo logo"
+            width={70}
+            quality={100}
+            placeholder="blur"
+            />
         <h1>DOJO Help desk</h1>
         <Link href="/">Dashboard</Link>
         <Link href="/tickets">tickets</Link>
