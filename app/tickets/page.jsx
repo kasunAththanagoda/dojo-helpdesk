d79d84f621cc5f0ac8096508d19000c1afae5bf8
kasun Aththanagoda <kasun@icanlk.com>
2024-06-02 10:36:08 +0530

    vid 6

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,7 +1,14 @@
+import TicketList from '@/app/tickets/ticketList'
 export default function Tickets(){
     return(
         <main>
-            tickets
+            <nav>
+                <div>
+                    <h2>Tickets</h2>
+                    <p><small>Currently open tickets.</small></p>
+                </div>
+            </nav>
+            <TicketList />
         </main>
     )
 }
