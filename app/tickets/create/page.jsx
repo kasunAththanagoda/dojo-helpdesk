7c2e13ee51852b26a81fc2bea756c37196bd7bad
kasun Aththanagoda <kasun@icanlk.com>
2024-06-03 17:55:30 +0530

    vid 11

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import CreateForm from "./CreateForm"
+export default function Create() {
+  return (
+    <main>
+    <h2 className="text-primary text-center">
+        Add a new ticket
+    </h2>
+    <CreateForm />
+</main>
+  )
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,13 +1,21 @@
 import TicketList from '@/app/tickets/ticketList'
 import { Suspense } from 'react'
 import Loading from '../loading'
+import Link from 'next/link'
+
 export default function Tickets(){
+
     return(
         <main>
-            <nav>
+            <nav className='flex justify-between'>
                 <div>
                     <h2>Tickets</h2>
                     <p><small>Currently open tickets.</small></p>
+                </div>
+                <div>
+                    <Link href='/tickets/create'>
+                    <button className='btn-primary'>Create new ticket</button>
+                    </Link>
                 </div>
             </nav>
             <Suspense>
